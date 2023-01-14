237e2575aeb1ea7c1e89665d777b4ed31218edce
sijan <sijansidda4@gmail.com>
2023-01-14 16:32:55 +0600

    Python1

---
+++ Even odd.py
@@ -0,0 +1,8 @@
+
+num=6
+
+if num%2==0:
+    print("Even")
+
+else:
+    print("ODD")
---
+++ elif statement.py
@@ -0,0 +1,16 @@
+
+Result=62
+
+if Result>=80:
+    print("A+")
+
+elif Result>=70:
+    print("B+")
+elif Result>=60:
+    print("C+")
+
+elif Result>=50:
+    print("D+")
+
+else:
+    print("Fail")
